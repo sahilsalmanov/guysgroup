be199ffc2130fb44b606412a6e889ec52164a564
sahilsalmanov <salmanovsahil7@gmail.com>
2024-03-10 09:47:46 +0400

    pc version finish

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"zzD++DdFqqrMUlfzurR9/Mr3gYStEccGrBKzxC1wSA0=\"}"
--- dist/static/chunks/app/layout-f8ecc0a6822e688a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4931:function(n,a,e){Promise.resolve().then(e.t.bind(e,7844,23))},7844:function(n){n.exports={style:{fontFamily:"'__Inter_aaf875', '__Inter_Fallback_aaf875'",fontStyle:"normal"},className:"__className_aaf875"}}},function(n){n.O(0,[971,69,744],function(){return n(n.s=4931)}),_N_E=n.O()}]);
